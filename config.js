296849fb1a57cd4984c16d056ca7a2da4512167f
Andrew800k <cazanin2015@yandex.ru>
2025-10-20 20:27:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const CONFIG = {
-    SITE_KEY: "0x4AAAAAABBYFE9qmmawHgI1",  // Site Key из Cloudflare
-    END_POINT: "https://tgcontest.cazanin2015.workers.dev/" // URL бэкенда
+    END_POINT: "https://tgcontest.duckdns.org/backend7" 
 };
 
 // Экспортируем объект CONFIG
